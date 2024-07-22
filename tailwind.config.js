80be78945702acb6ce8f1b059ed8d6949c0dee7a
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-07-22 22:46:42 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        Hijau: "#1C7547",
+        Dark: "#0f172a",
+        Pucat: "#64748b",
+        Dasar: "#F0E2B5",
+        Orange: "#FF6600",
+      },
+    },
+  },
+  plugins: [],
+};
